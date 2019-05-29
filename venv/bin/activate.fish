b961bc457fbe5700f90669e52c19ef92ac04c40b
Adrian Mindek <adrian.mindek@gmail.com>
2019-05-29 14:06:34 +0200

    Young Local runtime update
    Added venv requirments for running project

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lexaurin/Documents/Programovanie/Python/venv"
+set -gx VIRTUAL_ENV "/Users/lexaurin/Documents/GitHub/Python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/lexaurin/Documents/Programovanie/Python/venv/bin/python3
+#!/Users/lexaurin/Documents/GitHub/Python/venv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
